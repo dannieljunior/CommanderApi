d4cccf35447cd41fd91c07604cea6243ea15837e
Daniel Junior <daniel.junior@engegraph.com.br>
2020-07-08 16:59:45 -0300

    alteração "In" Dto

--- Dto/CommandInDto.cs
+++ Dto/CommandInDto.cs
@@ -1,11 +1,16 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Comander.Dto
 {
     public class CommandInDto
     {
+        [Required]
+        [MaxLength(250)]
         public string HowTo { get; set; }
+        [Required]
         public string Line { get; set; }
+        [Required]
         public string Plataform { get; set; }
     }
 }
